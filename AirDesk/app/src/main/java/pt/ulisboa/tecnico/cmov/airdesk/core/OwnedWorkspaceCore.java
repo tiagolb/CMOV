c02cc1b6314b53835be3ce6925279fe58e851958
dinismadeira <dinismadeira@gmail.com>
2015-04-08 19:55:14 +0100

    WorkspaceCore cleanup

--- AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/core/OwnedWorkspaceCore.java
+++ AirDesk/app/src/main/java/pt/ulisboa/tecnico/cmov/airdesk/core/OwnedWorkspaceCore.java
@@ -15,35 +15,4 @@ public class OwnedWorkspaceCore extends WorkspaceCore {
         super(name, quota, owner, isPublic);
     }
 
-    //load the workspaces
-    /*public static void loadWorkspaces(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        String workspacesJSONString = prefs.getString("ownedWorkspaces", "");
-        if (workspacesJSONString.equals(""))
-            OwnedWorkspaceCore.workspaces = new ArrayList<>();
-        else {
-            Type type = new TypeToken<ArrayList<OwnedWorkspaceCore>>() {
-            }.getType();
-            OwnedWorkspaceCore.workspaces = new Gson().fromJson(workspacesJSONString, type);
-        }
-    }
-
-    //store the workspaces
-    public static void saveWorkspaces(Context context) {
-        if (OwnedWorkspaceCore.workspaces != null) {
-            Type type = new TypeToken<ArrayList<OwnedWorkspaceCore>>() {
-            }.getType();
-            String workspacesJSONString = new Gson().toJson(OwnedWorkspaceCore.workspaces, type);
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-            prefs.edit().putString("ownedWorkspaces", workspacesJSONString).apply();
-        }
-    }*/
-
-
-    //get workspace by id
-    /*public static WorkspaceCore getWorkspaceById(String id) {
-        for (WorkspaceCore w : OwnedWorkspaceCore.workspaces)
-            if (w.getId().equals(id)) return w;
-        return null;
-    }*/
 }
